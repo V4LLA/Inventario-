50baa762d803c13e1898aca99820ac29603a23f7
V4LLA <77821844+V4LLA@users.noreply.github.com>
2021-05-06 19:06:03 -0500

    Update producto.js

--- producto.js
+++ producto.js
@@ -9,6 +9,7 @@ class Producto{
     info(){  
         return this.id + this.nombre + this.cantidad + this.precio;
     }
+
     infoHtml(){
         return `<div>
                     <h2>ID:${this.id}</h2>
@@ -16,5 +17,14 @@ class Producto{
                     <p>Cantidad:${this.cantidad}</p>
                     <p>Precio:${this.precio}</p>
                 </div>`;
+    }
+
+    infoTabla(){
+        return `<table>
+        <td> <strong>ID</strong> <br> ${this.id}</td>
+        <td> <strong>Nombre</strong> <br> ${this.nombre}</td>
+        <td> <strong>Cantidad</strong> <br> ${this.cantidad}</td>
+        <td> <strong>Precio</strong> <br> ${this.precio}</td>
+        </table>`;
     }
 }
